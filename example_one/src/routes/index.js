81fd916b62e011928b90c872a4c4461b2338d2ec
tpmccallum <mistermac2008@gmail.com>
2018-10-24 14:26:16 +1000

    Code cleanup and add variables

--- example_one/src/routes/index.js
+++ example_one/src/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CyberMiles DApp framework - example one' });
+    res.render('index', {
+        name: 'CyberMiles',
+        product: 'DApp framework example',
+        version: 'one'
+    });
 });
 
 module.exports = router;

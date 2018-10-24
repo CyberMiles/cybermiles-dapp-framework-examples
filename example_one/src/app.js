2958f8d9c618f1a688638528ce1ce6533bc49f7b
tpmccallum <mistermac2008@gmail.com>
2018-10-24 13:05:06 +1000

    Adding favicon as a test

--- example_one/src/app.js
+++ example_one/src/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//favicon
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.png')))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);

d71506f667c458061fbb66c6b63b5c15671c6d0a
tpmccallum <mistermac2008@gmail.com>
2018-10-31 10:17:06 +1000

    update web3 interaction

--- example_one/src/public/javascripts/blockchain_client.js
+++ example_one/src/public/javascripts/blockchain_client.js
@@ -3,20 +3,23 @@ var web3 = undefined;
 
 module.exports = {
     initBlockchain: function() {
-    	if (typeof web3 == 'undefined') {
-	        console.log('Initializing connection to the blockchain ...');
-	        web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
-	        console.log('Success!');
-	    } else {
-	    	console.log('Already initialized = ' + web3.currentProvider.isConnected());
-	    }
+        if (typeof web3 == 'undefined') {
+            console.log('Initializing connection to the blockchain ...');
+            //web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+            web3 = new Web3(new Web3.providers.HttpProvider("http://ec2-54-86-192-188.compute-1.amazonaws.com:8545"))
+            console.log('Success!');
+        } else {
+            console.log('Already initialized = ' + web3.currentProvider.isConnected());
+        }
 
         },
     isConnected: function() {
-    	console.log("web3.currentProvider.isConnected()");
-    	console.log(web3.currentProvider.isConnected());
+        console.log(web3.currentProvider.isConnected());
     },
-    getBlockchain: function() {
-    	return web3;
+    getAccounts: function() {
+        console.log(web3.cmt.accounts);
+    },
+    isSyncing(){
+        console.log(web3.cmt.syncing);
     }
 };
